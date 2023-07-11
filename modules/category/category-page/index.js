7416ce20040b4238e29a4db20beb843587e35345
rohandua <rohan.dua@squarepanda.com>
2023-07-11 13:42:40 +0530

    ECOM-19: limit no. of posts in a page

--- modules/category/category-page/index.js
+++ modules/category/category-page/index.js
@@ -2,7 +2,13 @@ module.exports = {
   extend: '@apostrophecms/piece-page-type',
   options: {
     label: 'Category Page',
-    pluralLabel: 'Categories Page'
+    pluralLabel: 'Categories Page',
+    pieceModuleName: 'category',
+    perPage: 8,
+    sort: {
+      date: -1,
+      lastPublishedAt: -1
+    }
   },
   fields: {
     add: {},
